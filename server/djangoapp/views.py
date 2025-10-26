9ffbdae5315565f6a65aca2e2ad3d05343a4e961
alexiscg17 <alexiscg_0417@hotmail.com>
2025-10-26 15:16:32 -0400

    Implement Dealer Reviews and fix errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,6 +94,7 @@ def get_dealer_reviews(request, dealer_id):
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
+            print("RESPONSE {} ".format(review_detail))
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
